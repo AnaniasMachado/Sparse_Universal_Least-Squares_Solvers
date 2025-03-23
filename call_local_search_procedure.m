dbd463ee9b6ba56eebd45fae260991755bad30a2
Ananias <86498679+AnaniasMachado@users.noreply.github.com>
2025-03-23 11:38:19 -0300

    Updating Python code 2 V2

---
+++ call_local_search_procedure.m
@@ -0,0 +1,46 @@
+function call_local_search_procedure(A_path, r, m, n, funcName, savePath, hatA_flag)
+    % Loads matrix A in the specified path
+    A = load(A_path);
+    
+    % Checks if A is a 2D matrix
+    if isfield(A, 'matrix')  % Checks if structure contains matrix
+        A = A.matrix;  % Atributes matrix if in structure
+        A = full(A); % Converts A to a dense matrix
+    end
+
+    if (hatA_flag == 1)
+        A = A' * A;
+        [m, n] = size(A);
+    end
+
+    addpath('Local_Search/nsub');
+    [R,C,time] = nsub(A,r);
+    
+    % Calls an appropriate function with given data parameters
+    switch funcName
+        case 'LSFI_Det'
+            addpath('Local_Search/P1');
+            [normLSFI_Det, timeLSFI_Det, swaps_LSFI_Det, R_LSFI_Det, C_LSFI_Det] = LSFI_Det(A, r, m, n, R, C);
+            A_inv = inv(A(R_LSFI_Det, C_LSFI_Det));
+            matrix = zeros(n, m);
+            matrix(C_LSFI_Det, R_LSFI_Det) = A_inv;
+        case 'LSFI_Det_Symmetric'
+            addpath('Local_Search/P1sym');
+            [norm, time, swaps, R] = LSFI_Det_Symmetric(A, r, m, R);
+            A_hat = A(R, R);
+            matrix = zeros(n, m);
+            matrix(R, R) = inv(A_hat);
+        case 'LSFI_Det_P3'
+            addpath('Local_Search/P13');
+            [norm, time, swaps, C_out] = LSFI_Det_P3(A, r, n, R, C);
+            A_hat = A(:, C_out);
+            matrix_hat = inv(A_hat' * A_hat) * A_hat';
+            matrix = zeros(n, m);
+            matrix(C_out, :) = matrix_hat;
+        otherwise
+            error('Function not recognized. Use "LSFI_Det", "LSFI_Det_Symmetric" or "LSFI_Det_P3".');
+    end
+    
+    % Saves resulting matrix in the specified path
+    save(savePath, 'matrix');
+end
---
+++ local_search.py
@@ -0,0 +1,40 @@
+import matlab.engine
+import os
+
+from utility import read_matrix, get_m_n_r_d_from_matrix_filepath
+
+def local_search_procedure(experiment, matrix_filepath, func_name, hatA_flag):
+    if func_name not in ["LSFI_Det", "LSFI_Det_Symmetric", "LSFI_Det_P3"]:
+        raise Exception("Error: Function does not exist.")
+
+    # Initiates Matlab engine
+    eng = matlab.engine.start_matlab()
+
+    # Gets current directory
+    current_directory = os.getcwd()
+
+    # Changes Matlab working directory
+    eng.cd(current_directory, nargout=0)
+
+    # Reads matrix
+    A = read_matrix(matrix_filepath)
+
+    # Gets matrix data
+    m, n, r, d = get_m_n_r_d_from_matrix_filepath(matrix_filepath)
+
+    # Specifies LS matrix name
+    LS_matrix_name = matrix_filepath.split("/")[-1].replace("matrix", "LS_matrix")
+
+    # Specifies path to save LS matrix
+    save_path = f"Local_Search_Matrices/Experiment_{experiment}/{LS_matrix_name}"
+
+    # Calls Local Search Procedure
+    eng.call_local_search_procedure(matrix_filepath, r, m, n, func_name, save_path, hatA_flag, nargout=0)
+
+    # Reads LS matrix
+    LS_matrix = read_matrix(save_path)
+
+    # Ends Matlab engine
+    eng.quit()
+
+    return LS_matrix
