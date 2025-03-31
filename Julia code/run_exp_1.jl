6016b83526951f3aea8944ac42493ce71df5ff91
AnaniasMachado <86498679+AnaniasMachado@users.noreply.github.com>
2025-03-30 20:38:42 -0400

    Sending results of redoing experiment 1

--- Julia code/run_exp_1.jl
+++ Julia code/run_exp_1.jl
@@ -27,19 +27,16 @@ P1_P3_H_norm_0_list = []
 P1_P3_H_norm_1_list = []
 P1_P3_H_rank_list = []
 P1_P3_time_list = []
-P1_P3_memory_list = []
 
 PLS_H_norm_0_list = []
 PLS_H_norm_1_list = []
 PLS_H_rank_list = []
 PLS_time_list = []
-PLS_memory_list = []
 
 count = 0
 
-# for mat_file in mat_files[41:end]
 for mat_file in mat_files
-    count += 1
+    global count += 1
 
     mat_path = joinpath(matrices_folder, mat_file)
     mat_data = matread(mat_path)
@@ -64,52 +61,42 @@ for mat_file in mat_files
 
         constraints = ["P1", "P3"]
 
-        VM1 = get_vmsize()
         GRB_P1_P3_time = @elapsed begin
             GRB_P1_P3_H = gurobi_solver(data, constraints, opt_tol)
         end
-        VM2 = get_vmsize()
         GRB_P1_P3_H_norm_0 = matrix_norm_0(GRB_P1_P3_H)
         GRB_P1_P3_H_norm_1 = norm(GRB_P1_P3_H, 1)
         GRB_P1_P3_H_rank = calculate_rank(GRB_P1_P3_H)
-        GRB_P1_P3_memory = VM2 - VM1
 
-        push!(P1_P3_H_norm_0_list, GRB_P1_H_norm_0)
+        push!(P1_P3_H_norm_0_list, GRB_P1_P3_H_norm_0)
         push!(P1_P3_H_norm_1_list, GRB_P1_P3_H_norm_1)
         push!(P1_P3_H_rank_list, GRB_P1_P3_H_rank)
         push!(P1_P3_time_list, GRB_P1_P3_time)
-        push!(P1_P3_memory_list, GRB_P1_P3_memory)
 
         constraints = ["PLS"]
 
-        VM1 = get_vmsize()
         GRB_PLS_time = @elapsed begin
             GRB_PLS_H = gurobi_solver(data, constraints, opt_tol)
         end
-        VM2 = get_vmsize()
         GRB_PLS_H_norm_0 = matrix_norm_0(GRB_PLS_H)
         GRB_PLS_H_norm_1 = norm(GRB_PLS_H, 1)
         GRB_PLS_H_rank = calculate_rank(GRB_PLS_H)
-        GRB_PLS_memory = VM2 - VM1
 
         push!(PLS_H_norm_0_list, GRB_PLS_H_norm_0)
         push!(PLS_H_norm_1_list, GRB_PLS_H_norm_1)
         push!(PLS_H_rank_list, GRB_PLS_H_rank)
         push!(PLS_time_list, GRB_PLS_time)
-        push!(PLS_memory_list, GRB_PLS_memory)
 
         if count % 5 == 0
             GRB_P1_P3_H_norm_0_mean = mean(P1_P3_H_norm_0_list)
             GRB_P1_P3_H_norm_1_mean = mean(P1_P3_H_norm_1_list)
-            GRB_P1_P3_H_rank_mean = mean(P1_P3_H_rank_list)
+            GRB_P1_P3_H_rank_mean = round(Int, mean(P1_P3_H_rank_list))
             GRB_P1_P3_time_mean = mean(P1_P3_time_list)
-            GRB_P1_P3_memory_mean = mean(P1_P3_memory_list)
 
             GRB_PLS_H_norm_0_mean = mean(PLS_H_norm_0_list)
             GRB_PLS_H_norm_1_mean = mean(PLS_H_norm_1_list)
-            GRB_PLS_H_rank_mean = mean(PLS_H_rank_list)
+            GRB_PLS_H_rank_mean = round(Int, mean(PLS_H_rank_list))
             GRB_PLS_time_mean = mean(PLS_time_list)
-            GRB_PLS_memory_mean = mean(PLS_memory_list)
 
             result = DataFrame(
                 m = [m],
@@ -120,27 +107,23 @@ for mat_file in mat_files
                 GRB_P1_P3_H_norm_1_mean = [GRB_P1_P3_H_norm_1_mean],
                 GRB_P1_P3_H_rank_mean = [GRB_P1_P3_H_rank_mean],
                 GRB_P1_P3_time_mean = [GRB_P1_P3_time_mean],
-                GRB_P1_P3_memory_mean = [GRB_P1_P3_memory_mean]
                 GRB_PLS_H_norm_0_mean = [GRB_PLS_H_norm_0_mean],
                 GRB_PLS_H_norm_1_mean = [GRB_PLS_H_norm_1_mean],
                 GRB_PLS_H_rank_mean = [GRB_PLS_H_rank_mean],
-                GRB_PLS_time_mean = [GRB_PLS_time_mean],
-                GRB_PLS_memory_mean = [GRB_PLS_memory_mean]
+                GRB_PLS_time_mean = [GRB_PLS_time_mean]
             )
 
             append!(df, result)
 
-            P1_P3_H_norm_0_list = []
-            P1_P3_H_norm_1_list = []
-            P1_P3_H_rank_list = []
-            P1_P3_time_list = []
-            P1_P3_memory_list = []
+            empty!(P1_P3_H_norm_0_list)
+            empty!(P1_P3_H_norm_1_list)
+            empty!(P1_P3_H_rank_list)
+            empty!(P1_P3_time_list)
 
-            PLS_H_norm_0_list = []
-            PLS_H_norm_1_list = []
-            PLS_H_rank_list = []
-            PLS_time_list = []
-            PLS_memory_list = []
+            empty!(PLS_H_norm_0_list)
+            empty!(PLS_H_norm_1_list)
+            empty!(PLS_H_rank_list)
+            empty!(PLS_time_list)
         end
 
         GC.gc()
--- Julia code/utility.jl
+++ Julia code/utility.jl
@@ -13,20 +13,29 @@ function gen_random_rank_r_matrix(m::Int64, n::Int64, r::Int64)
     return A
 end
 
+# function matrix_norm_0(A::Matrix{Float64})
+#     norm_0 = count(x -> abs(x) > epsilon, A)
+#     return norm_0
+# end
+
 function matrix_norm_0(A::Matrix{Float64})
-    norm_0 = count(x -> abs(x) > epsilon, A)
+    norm_0 = 0
+    for a in A
+        if abs(a) > epsilon
+            norm_0 += 1
+        end
+    end
     return norm_0
 end
 
 function calculate_rank(A::Matrix{Float64})
     U, S, V = svd(A)
     S = Diagonal(S)
-    rank = count(x -> abs(x) > epsilon, S)
+    rank = 0
+    for s in S
+        if abs(s) > epsilon
+            rank += 1
+        end
+    end
     return rank
-end
-
-function get_vmsize()
-    ru = Vector{RUsage}(undef, 1)
-    ccall(:getrusage, Cint, (Cint, Ptr{Cvoid}), 0, ru)
-    return ru[1].ru_maxrss
 end
