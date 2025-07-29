ea2f9da278fec91b3bcba789495228830d2fe3b2
AnaniasMachado <86498679+AnaniasMachado@users.noreply.github.com>
2025-07-29 09:08:30 -0400

    Updating plots from experiments 9-11

--- Julia code/run_exp_10_plots.jl
+++ Julia code/run_exp_10_plots.jl
@@ -1,6 +1,7 @@
 using CSV
 using DataFrames
 using Plots
+using LaTeXStrings
 
 grb_exp = 10
 drs_exp = 1
@@ -22,49 +23,49 @@ df_admm= CSV.read(admm_fp_filepath, DataFrame)
 df_grb = filter(row -> row[:time_mean] != -1, df_grb)
 sort!(df_grb, :m)
 
-plot(df_grb.m, df_grb.time_mean, label="Gurobi", xlabel="m", ylabel="time(s)", title="m x time")
+plot(df_grb.m, df_grb.time_mean, label=L"\textrm{Gurobi}", xlabel=L"m", ylabel=L"\textrm{time(s)}")
 
-plot!(df_drs.m, df_drs.time_mean, label="DRS")
+plot!(df_drs.m, df_drs.time_mean, label=L"\textrm{DRS}^{\epsilon}")
 
-plot!(df_drs_boyd.m, df_drs_boyd.time_mean, label="DRS_Boyd")
+plot!(df_drs_boyd.m, df_drs_boyd.time_mean, label=L"\textrm{DRS}^{\epsilon(r^0)}")
 
-plot!(df_drs_fp.m, df_drs_fp.time_mean, label="DRS_FP")
+plot!(df_drs_fp.m, df_drs_fp.time_mean, label=L"\textrm{DRS}^{\textrm{FP}}")
 
-plot!(df_admme.m, df_admme.time_mean, label="ADMM^epsilon")
+plot!(df_admme.m, df_admme.time_mean, label=L"\textrm{ADMM}^{\epsilon}")
 
-plot!(df_admm.m, df_admm.time_mean, label="ADMM")
+plot!(df_admm.m, df_admm.time_mean, label=L"\textrm{ADMM}^{\epsilon(r^k)}")
 
 # display(current())
 
 savefig("Plots/Experiment_$(grb_exp)/exp_$(grb_exp)_time_plot.png")
 
-plot(df_grb.m, df_grb.H_div_AMP_norm_0_mean, label="Gurobi", xlabel="m", ylabel="||H||_0/||AMP||_0", title="m x ||H||_0/||AMP||_0")
+plot(df_grb.m, df_grb.H_div_AMP_norm_0_mean, label=L"\textrm{Gurobi}", xlabel=L"m", ylabel=L"||\textrm{H}||_0/||\textrm{AMP}||_0")
 
-plot!(df_drs.m, df_drs.H_div_AMP_norm_0_mean, label="DRS")
+plot!(df_drs.m, df_drs.H_div_AMP_norm_0_mean, label=L"\textrm{DRS}^{\epsilon}")
 
-plot!(df_drs_boyd.m, df_drs_boyd.H_div_AMP_norm_0_mean, label="DRS_Boyd")
+plot!(df_drs_boyd.m, df_drs_boyd.H_div_AMP_norm_0_mean, label=L"\textrm{DRS}^{\epsilon(r^0)}")
 
-plot!(df_drs_fp.m, df_drs_fp.H_div_AMP_norm_0_mean, label="DRS_FP")
+plot!(df_drs_fp.m, df_drs_fp.H_div_AMP_norm_0_mean, label=L"\textrm{DRS}^{\textrm{FP}}")
 
-plot!(df_admme.m, df_admme.H_div_AMP_norm_0_mean, label="ADMM^epsilon")
+plot!(df_admme.m, df_admme.H_div_AMP_norm_0_mean, label=L"\textrm{ADMM}^{\epsilon}")
 
-plot!(df_admm.m, df_admm.H_div_AMP_norm_0_mean, label="ADMM")
+plot!(df_admm.m, df_admm.H_div_AMP_norm_0_mean, label=L"\textrm{ADMM}^{\epsilon(r^k)}")
 
 # display(current())
 
 savefig("Plots/Experiment_$(grb_exp)/exp_$(grb_exp)_norm_0_ratio_plot.png")
 
-plot(df_grb.m, df_grb.H_div_AMP_norm_1_mean, label="Gurobi", xlabel="m", ylabel="||H||_1/||AMP||_1", title="m x ||H||_1/||AMP||_1")
+plot(df_grb.m, df_grb.H_div_AMP_norm_1_mean, label=L"\textrm{Gurobi}", xlabel=L"m", ylabel=L"||\textrm{H}||_1/||\textrm{AMP}||_1")
 
-plot!(df_drs.m, df_drs.H_div_AMP_norm_1_mean, label="DRS")
+plot!(df_drs.m, df_drs.H_div_AMP_norm_1_mean, label=L"\textrm{DRS}^{\epsilon}")
 
-plot!(df_drs_boyd.m, df_drs_boyd.H_div_AMP_norm_1_mean, label="DRS_Boyd")
+plot!(df_drs_boyd.m, df_drs_boyd.H_div_AMP_norm_1_mean, label=L"\textrm{DRS}^{\epsilon(r^0)}")
 
-plot!(df_drs_fp.m, df_drs_fp.H_div_AMP_norm_1_mean, label="DRS_FP")
+plot!(df_drs_fp.m, df_drs_fp.H_div_AMP_norm_1_mean, label=L"\textrm{DRS}^{\textrm{FP}}")
 
-plot!(df_admme.m, df_admme.H_div_AMP_norm_1_mean, label="ADMM^epsilon")
+plot!(df_admme.m, df_admme.H_div_AMP_norm_1_mean, label=L"\textrm{ADMM}^{\epsilon}")
 
-plot!(df_admm.m, df_admm.H_div_AMP_norm_1_mean, label="ADMM")
+plot!(df_admm.m, df_admm.H_div_AMP_norm_1_mean, label=L"\textrm{ADMM}^{\epsilon(r^k)}")
 
 # display(current())
 
---
+++ Julia code/test_run_drs.jl
@@ -0,0 +1,52 @@
+using MAT
+
+include("utility.jl")
+include("types.jl")
+include("drs.jl")
+
+m = 100
+n = 50
+r = 25
+
+A = gen_random_rank_r_matrix(m, n, r)
+
+matrix_folder = "./Experiment_Matrices/DRS_Experiment_2"
+mat_files = readdir(matrix_folder)
+mat_file = mat_files[1]
+mat_path = joinpath(matrix_folder, mat_file)
+mat_data = matread(mat_path)
+A = mat_data["matrix"]
+A = Matrix(A)
+AMP = pinv(A)
+m, n = size(A)
+r = rank(A)
+
+println("m = $m, n = $n, r = $r")
+
+data = DataInst(A, m, n, r, AMP=AMP)
+constraints = ["PLS"]
+problem = "PLS"
+eps_opt = 10^(-5)
+rho = 3.0
+lambda = 10^(-2)
+
+epsilon = 10^(-12)
+eps_abs = epsilon
+eps_rel = epsilon
+fixed_tol = false
+
+time_limit = 1200
+stop_crits = ["Boyd", "Fixed_Point"]
+stop_crit = stop_crits[1]
+
+DRS_time = @elapsed begin
+    DRS_H, DRS_k = drs(A, lambda, eps_abs, eps_rel, problem, fixed_tol, eps_opt, stop_crit, time_limit)
+end
+DRS_H_norm_0 = matrix_norm_0(DRS_H)
+DRS_H_norm_1 = norm(DRS_H, 1)
+
+println("DRS time: $DRS_time")
+println("DRS k: $DRS_k")
+println("DRS norm 1: $DRS_H_norm_1")
+println("DRS norm 0: $DRS_H_norm_0")
+println("DRS bound ratio: $(DRS_H_norm_0 / (m * r))")
